d8f908a1659cc08079327edbbd38b1011e2c59a8
Radivarig <reslav.hollos@gmail.com>
2015-01-22 20:12:33 +0100

    [chg]set display to none [add]skip already hidden [add]counter

--- data/hide-comments.js
+++ data/hide-comments.js
@@ -1,15 +1,20 @@
 function hasClass(element, cls){
 	return (' ' +element.className +' ').indexOf(' ' +cls +' ') > -1
 }
+var cnt = 0
 var profileLinks = document.getElementsByClassName("profileLink")
 for(var i = 0; i < profileLinks.length; ++i){
 	var p = profileLinks[i]
 	if (hasClass (p.parentNode, "UFICommentBody")){
 	    p = p.parentNode.parentNode
 		while (p) {
-	    if ( hasClass (p, "UFIComment"))
-				 p.setAttribute("style", "border: solid red 3px;")
+	    if ( hasClass (p, "UFIComment") && p.style.display != "none"){
+	    	p.style.display = "none"
+	    	//p.setAttribute("style", "display: none;")
+	    	cnt += 1
+	    }
 	    p = p.parentNode
 		}
 	}
 }
+console.error("@@@ nuked ", +cnt + " noobs.")
