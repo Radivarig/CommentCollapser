47c776dbda68064289219009985fef685f4e7b67
Radivarig <reslav.hollos@gmail.com>
2015-01-22 19:28:08 +0100

    [add]detection of comment containing profile link

--- data/hide-comments.js
+++ data/hide-comments.js
@@ -1,15 +1,15 @@
-var elements = document.getElementsByClassName("UFIComment")
-for (var i = 0; i < elements.length; i++) {
-	//self.port.emit("gotElement", elements[i])
-	//console.log(elements[i].innerHTML)
-	//elements[i].parentNode.removeChild(elements[i])
-	elements[i].setAttribute("style", "border: solid red 1px;")
+function hasClass(element, cls){
+	return (' ' +element.className +' ').indexOf(' ' +cls +' ') > -1
 }
-
-var elements = document.getElementsByClassName("profileLink")
-for (var i = 0; i < elements.length; i++) {
-	//self.port.emit("gotElement", elements[i])
-	//console.log(elements[i].innerHTML)
-	//elements[i].parentNode.removeChild(elements[i])
-	elements[i].setAttribute("style", "border: solid blue 3px;")
+var profileLinks = document.getElementsByClassName("profileLink")
+for(var i = 0; i < profileLinks.length; ++i){
+	var p = profileLinks[i]
+	if (hasClass (p.parentNode, "UFICommentBody")){
+	    p = p.parentNode.parentNode
+		while (p) {
+	    if ( hasClass (p, "UFIComment"))
+				 p.setAttribute("style", "border: solid red 3px;")
+	    p = p.parentNode
+		}
+	}
 }
