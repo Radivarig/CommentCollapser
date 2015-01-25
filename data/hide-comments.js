6f4a78aeef5e0d084e35095d0dae98c3f6e0eb00
Radivarig <reslav.hollos@gmail.com>
2015-01-25 19:09:18 +0100

    [fix]injected css directly to head

--- data/hide-comments.js
+++ data/hide-comments.js
@@ -1,10 +1,17 @@
 //TODO remove comments of removed comment
 
-/*var l = document.createElement("link")
+/*var l = document.createElement("style")
 l.setAttribute("rel", "stylesheet")
 l.setAttribute("type", "text/css")
 l.setAttribute("href", chrome://url/to/anims.css) //TODO setup chrome.manifest
 document.getElementsByTagName("head")[0].appendChild(l)*/
+
+var css = 'a.profileLink { color: #333;	background-color: #0c0;	animation: profilelinkanim 1s ease 3; } @keyframes profilelinkanim { 50% { opacity: 0; } }'
+var head = document.head
+var style = document.createElement('style')
+style.type = 'text/css'
+style.appendChild(document.createTextNode(css))
+head.appendChild(style)
 
 var hasClass = function(element, cls){
   return (' ' +element.className +' ').indexOf(' ' +cls +' ') > -1
@@ -26,13 +33,10 @@ var HideComments = function(){
   for(var i = 0; i < profileLinks.length; ++i)
     HideCommentOfProfileLink(profileLinks[i])
 }
-HideComments()
 
 var cb = function(event){
 	if (event.animationName === 'profilelinkanim')
-		console.error("@@@ animation event", event.target)
-		//HideCommentOfProfileLink(event.target)
-
+		HideCommentOfProfileLink(event.target)
 }
 document.body.addEventListener('animationstart', cb, false)
 
