a2a1c65f48243f7c3e7a7fa59376739e759f4ec5
Radivarig <reslav.hollos@gmail.com>
2015-01-25 18:51:55 +0100

    [chg]separated to smaller functions [add]animation event listener

--- data/hide-comments.js
+++ data/hide-comments.js
@@ -1,20 +1,51 @@
-function hasClass(element, cls){
-	return (' ' +element.className +' ').indexOf(' ' +cls +' ') > -1
+//TODO remove comments of removed comment
+
+/*var l = document.createElement("link")
+l.setAttribute("rel", "stylesheet")
+l.setAttribute("type", "text/css")
+l.setAttribute("href", chrome://url/to/anims.css) //TODO setup chrome.manifest
+document.getElementsByTagName("head")[0].appendChild(l)*/
+
+var hasClass = function(element, cls){
+  return (' ' +element.className +' ').indexOf(' ' +cls +' ') > -1
 }
-var cnt = 0
-var profileLinks = document.getElementsByClassName("profileLink")
-for(var i = 0; i < profileLinks.length; ++i){
-	var p = profileLinks[i]
+
+var HideCommentOfProfileLink = function(p){
 	if (hasClass (p.parentNode, "UFICommentBody")){
-	    p = p.parentNode.parentNode
-		while (p) {
-	    if ( hasClass (p, "UFIComment") && p.style.display != "none"){
-	    	p.style.display = "none"
-	    	//p.setAttribute("style", "display: none;")
-	    	cnt += 1
-	    }
+	  p = p.parentNode.parentNode
+	  while (p) {
+	    if ( hasClass (p, "UFIComment") && p.style.display != "none")
+	      p.style.display = "none"
 	    p = p.parentNode
-		}
+	  }
 	}
 }
-//console.error("@@@ nuked " +cnt + " noobs.")
+
+var HideComments = function(){
+  var profileLinks = document.getElementsByClassName("profileLink")
+  for(var i = 0; i < profileLinks.length; ++i)
+    HideCommentOfProfileLink(profileLinks[i])
+}
+HideComments()
+
+var cb = function(event){
+	if (event.animationName === 'profilelinkanim')
+		console.error("@@@ animation event", event.target)
+		//HideCommentOfProfileLink(event.target)
+
+}
+document.body.addEventListener('animationstart', cb, false)
+
+/*
+var observer = new MutationObserver( cb )
+
+var target = document.body
+var config = {
+	childList: true
+	,attributes: true
+	,subtree: true
+	,attributeFilter: ["UFIList"]
+}
+
+observer.observe(target, config)
+//observer.disconnect()*/
