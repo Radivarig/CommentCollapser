c7cb23256246a9ae2bcc6a0319a3f4f9414fcca5
Radivarig <reslav.hollos@gmail.com>
2015-01-27 06:51:55 +0100

    [add]hide collapsed

---
+++ data/hide-collapsed.js
@@ -0,0 +1,5 @@
+var collapsed = document.querySelectorAll('.UFIDetected')
+for(var i = 0; i < collapsed.length; ++i){
+	collapsed[i].style.display = "none"
+	//collapsed[i].classList.remove("UFIDetected")
+}
